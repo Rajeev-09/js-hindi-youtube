7fb18beb463f87560a3da6e17050943cfce8fbc9
Rajeev-09 <140807169+Rajeev-09@users.noreply.github.com>
2024-01-14 13:27:43 +0000

    Conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,38 @@
+let score = null
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/* * convert 
+ * "33" => 33 
+ * "33acn" => Nan
+ * true => 1 or fasle => 0
+ */
+
+
+// * conversion of 1 into the Boolean Operation
+
+// let isloggedIn = 1
+// let booleanIsLoggedIn = Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// In the  
+
+let isloggedIn = ""
+let booleanIsLoggedIn = Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 99
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
