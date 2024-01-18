e2fc5473b740cc167d719d7530393577b76f6645
Rajeev-09 <140807169+Rajeev-09@users.noreply.github.com>
2024-01-18 18:48:37 +0000

    date in js

---
+++ 01_basic/07_dateinJs.js
@@ -0,0 +1,36 @@
+// Dates
+
+// let myDate = new Date()
+// // console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2020,0,23)
+// let myCreatedDate = new Date(2020,0,23,5,3)
+// let myCreatedDate = new Date("2024-01-23")
+let myCreatedDate = new Date("01-23-2024")
+
+// console.log(myCreatedDate.toLocaleString());
+
+
+let timeStamp = Date.now()
+// console.log(timeStamp);
+// console.log(myCreatedDate.getTime
+// console.log(Math.floor(Date.now()/1000))
+
+
+let NewDate = new Date()
+console.log(NewDate);
+console.log(NewDate.getMonth());
+console.log(NewDate.getDay());
+
+// customize the date using local string
+NewDate.toLocaleString('default',{
+    weekday: "short",
+    
+})
+
+
+
+
